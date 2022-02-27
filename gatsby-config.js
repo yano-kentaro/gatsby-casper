a3a25a67b98c4440812fb193da5d27ea25a2f3fb
yano-kentaro <ynkn0829@gmail.com>
2022-02-28 00:39:21 +0900

    GA trackingID設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,7 +123,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'G-0RNHTZJNPT',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
