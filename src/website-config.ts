d4a144742c9ed640649fe648a8e16846aa11b9a4
yano-kentaro <ynkn0829@gmail.com>
2022-02-28 01:38:03 +0900

    サイト情報の変更

--- src/website-config.ts
+++ src/website-config.ts
@@ -55,14 +55,14 @@ export interface WebsiteConfig {
 
 const config: WebsiteConfig = {
   title: 'Ghost',
-  description: 'The professional publishing platform',
+  description: '札幌WEB系エンジニアの技術ブログ',
   coverImage: 'img/blog-cover.png',
-  logo: 'img/ghost-logo.png',
-  lang: 'en',
+  logo: 'img/kengineer-dev-logo2.png',
+  lang: 'ja',
   siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
-  showSubscribe: true,
+  // facebook: 'https://www.facebook.com/ghost',
+  twitter: 'https://twitter.com/_KEngineer_',
+  showSubscribe: false,
   mailchimpAction:
     'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
